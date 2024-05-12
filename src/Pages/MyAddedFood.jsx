9d27058bca9a79dc402f7397929076c79f876a32
Adibkhan619 <work.adibkhan@gmail.com>
2024-05-12 11:55:07 +0600

    Add delete functionality in my order list

--- src/Pages/MyAddedFood.jsx
+++ src/Pages/MyAddedFood.jsx
@@ -6,13 +6,100 @@ const MyAddedFood = () => {
     const food = useLoaderData()
     console.log(food);
 
-
+    // const  {
+    //     // _id,
+    //     food_name,
+    //     food_image,
+    //     food_category,
+    //     price,
+    //     made_by,
+    //     food_origin,
+    //     email,
+    //     description,
+    //     quantity,
+    //     order_count,
+    // } = food;
+    // console.log(food);
 
 
 
     return (
         <div>
-            
+                <div className="lg:mx-10">
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Category</th>
+                            <th>Quantity</th>
+                            <th>Made by</th>
+                            {/* <th>Date</th> */}
+                            <th>Price</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+{
+                    food.map(food => (
+<tr key={food._id}>
+                            <td className="w-[500px]">
+                                <div className="flex items-center gap-3">
+                                    <div className="avatar">
+                                        <div className=" w-48 h-32">
+                                            <img className="w-16 h-32 rounded-lg"
+                                                src={food.food_image}
+                                                alt="Avatar Tailwind CSS Component"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="font-bold text-2xl">
+                                            {food.food_name}
+                                        </div>
+                                        <div className="text-sm opacity-50">
+                                            {food.food_origin}
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                               {food.food_category}
+                                <br />
+                                <span className="badge badge-ghost badge-sm" title={food.description}>
+                                    
+                                    <span>{(food.description).split("").slice(0, 30).join("")}</span>
+                                </span>
+                            </td>
+                            <td>{food.order_count}</td>
+                            <td>{food.made_by}</td>
+                            {/* <td>{order.date}</td> */}
+                            <td>{food.price}</td>
+                            <th>
+                                <button className="btn btn-ghost btn-xs">
+                                    Remove
+                                </button>
+                            </th>
+                        </tr>
+                    ))
+        }                       
+                    </tbody>
+                    {/* foot */}
+                    <tfoot>
+                        <tr>
+                        <th>Name</th>
+                            <th>Category</th>
+                            <th>Quantity</th>
+                            <th>Made by</th>
+                            {/* <th>Date</th> */}
+                            <th>Price</th>
+                            <th></th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
         </div>
     );
 };
--- src/Pages/MyOrders.jsx
+++ src/Pages/MyOrders.jsx
@@ -1,8 +1,12 @@
 // import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { useLoaderData } from "react-router-dom";
 
 const MyOrders = () => {
-    const myOrders = useLoaderData();
+    const [items, setItems] = useState([])
+    // const myOrders = useLoaderData();
     // const [orders, setOrders] = useState([]);
     // const {
     //     name,
@@ -19,9 +23,32 @@ const MyOrders = () => {
     //     available,
     // } = myOrders;
     // setOrders(myOrders);
-    console.log(myOrders);
+    // console.log(myOrders);
+    
+    useEffect(() => {
+        getData()
+      }, [])
+    
+      const getData = async () => {
+        const { data } = await axios(`http://localhost:5000/orders`)
+        setItems(data)
+      }
+
+
+    const handleDelete = async id => {
+        try {
+          const { data } = await axios.delete(`http://localhost:5000/order/${id}`)
+          console.log(data)
+          toast.success('Delete Successful')
+    
+          getData()
+        } catch (err) {
+          console.log(err.message)
+          toast.error(err.message)
+        }
+      }
     return (
-    <div className="lg:mx-10">
+        <div className="lg:mx-10">
             <div className="overflow-x-auto">
                 <table className="table">
                     {/* head */}
@@ -37,53 +64,58 @@ const MyOrders = () => {
                         </tr>
                     </thead>
                     <tbody>
-{
-                    myOrders.map(order => (
-<tr key={order._id}>
-                            <td>
-                                <div className="flex items-center gap-3">
-                                    <div className="avatar">
-                                        <div className="mask mask-squircle w-12 h-12">
-                                            <img
-                                                src={order.image}
-                                                alt="Avatar Tailwind CSS Component"
-                                            />
+                        {items.map((order) => (
+                            <tr key={order._id}>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img
+                                                    src={order.image}
+                                                    alt="Avatar Tailwind CSS Component"
+                                                />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">
+                                                {order.name}
+                                            </div>
+                                            <div className="text-sm opacity-50">
+                                                {order.origin}
+                                            </div>
                                         </div>
                                     </div>
-                                    <div>
-                                        <div className="font-bold">
-                                            {order.name}
-                                        </div>
-                                        <div className="text-sm opacity-50">
-                                            {order.origin}
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                               {order.category}
-                                <br />
-                                <span className="badge badge-ghost badge-sm" title={order.foodDescription}>
-                                    {(order.foodDescription).split("").slice(0, 30).join("")}<span>...</span>
-                                </span>
-                            </td>
-                            <td>{order.orderQuantity}</td>
-                            <td>{order.chef}</td>
-                            <td>{order.date}</td>
-                            <td>{order.price}</td>
-                            <th>
-                                <button className="btn btn-ghost btn-xs">
-                                    Remove
-                                </button>
-                            </th>
-                        </tr>
-                    ))
-        }                       
+                                </td>
+                                <td>
+                                    {order.category}
+                                    <br />
+                                    <span
+                                        className="badge badge-ghost badge-sm"
+                                        title={order.foodDescription}
+                                    >
+                                        {order.foodDescription
+                                            .split("")
+                                            .slice(0, 30)
+                                            .join("")}
+                                        <span>...</span>
+                                    </span>
+                                </td>
+                                <td>{order.orderQuantity}</td>
+                                <td>{order.chef}</td>
+                                <td>{order.date}</td>
+                                <td>{order.price}</td>
+                                <th>
+                                    <button onClick={() => handleDelete(order._id)} className="btn btn-ghost btn-xs">
+                                        Remove
+                                    </button>
+                                </th>
+                            </tr>
+                        ))}
                     </tbody>
                     {/* foot */}
                     <tfoot>
                         <tr>
-                        <th>Name</th>
+                            <th>Name</th>
                             <th>Category</th>
                             <th>Quantity</th>
                             <th>Made by</th>
